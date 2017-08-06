b2d1108fc507fbf186d484dd76933bf17de03e86
Martin Bradac <martin.bradac@email.cz>
2017-08-06 09:00:27 +0200

    Added functionality for the buttons and decision logic

--- app/src/main/java/space/android_dev/withdrawingconverter/MainActivity.java
+++ app/src/main/java/space/android_dev/withdrawingconverter/MainActivity.java
@@ -2,8 +2,50 @@ package space.android_dev.withdrawingconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void convertFunction (View view) {
+
+        EditText inputForeignCurr = (EditText) findViewById(R.id.inputForeignCurr);
+        EditText inputHomeCurr = (EditText) findViewById(R.id.inputHomeCurr);
+
+        String inForeignCurr = inputForeignCurr.getText().toString();
+        String inHomeCurr = inputHomeCurr.getText().toString();
+
+        Double rateCZKtoEUR =  0.038;
+        Double rateEURtoCZK =  26.11;
+
+        if (inForeignCurr.isEmpty() && !inHomeCurr.isEmpty()) {
+            Double HomeCurr = Double.parseDouble(inputHomeCurr.getText().toString());
+            Double res = HomeCurr * rateCZKtoEUR;
+            inputForeignCurr.setText(res.toString());
+
+        } else if (inHomeCurr.isEmpty() && !inForeignCurr.isEmpty()) {
+            Double ForreignCurr = Double.parseDouble(inputForeignCurr.getText().toString());
+            Double res = ForreignCurr *rateEURtoCZK;
+            inputHomeCurr.setText(res.toString());
+
+        } else if (!inForeignCurr.isEmpty() && !inHomeCurr.isEmpty()) {
+            Toast.makeText(getApplicationContext(),"Both inputs cannot be inserted !", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(getApplicationContext(),"Nothing inserted !", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public void clearFunction (View view){
+        EditText inputForeignCurr = (EditText) findViewById(R.id.inputForeignCurr);
+        EditText inputHomeCurr = (EditText) findViewById(R.id.inputHomeCurr);
+
+        inputForeignCurr.setText("");
+        inputHomeCurr.setText("");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
